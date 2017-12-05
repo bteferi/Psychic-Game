deb70a8781c14762f580afa386b516d6337a9838
Beza <bezawit.teferi@gmail.com>
2017-12-04 20:11:18 -0500

    game is functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,59 @@
 // show a message that days Guess what letter Im thinking
-var win=0;
-var losses=0;
-var RemainingGuess=3;
+var win = 0;
+var losses = 0;
+var RemainingGuess = 5;
+var UserGuess = '';
+var AllUser_Guess = [];
+var Computer_Choice_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ComputerGuess = Computer_Choice_list[Math.floor(Math.random() * Computer_Choice_list.length)];
+console.log(ComputerGuess);
 // 2.0 Problem How do I get users guess
 //2.0 I need an event listner to get users input
 //using key presse info in to variable for later use
 // this funcion is run when ever the user presses a key
 
-   document.onkeyup = function(event) {
-      var UserGuess=event.key;
+// here is the code shown at the start of the html - this will not update because it is not in the function//
+document.querySelector("#RemainingGuess").innerHTML = RemainingGuess; // here it is not updating
+document.querySelector("#losses_counter").innerHTML = losses;
+document.querySelector("#win_counter").innerHTML = win;
 
-//3.0 Problem : How do I get the computers choice ?
-// I should have a variable that holds an array of the alphabet.
-        var Computer_Choice_list =["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-//4.0 Problem : how can i pick a random number from the array
-//4.0 solution create another variable to create a random pick from the array containing the alpabet but to pick one randomly
-        var ComputerGuess = Computer_Choice_list[Math.floor(Math.random() * Computer_Choice_list.length)];
-//5.0 Problem compare guesses to one another and determin results
-//5.0 solution : use a forloop and inside it an if else Use conditional statments
 
-  if (UserGuess == ComputerGuess){ // if the two are equal - then update the win var by one
-    win++;
-// else keep running the game until the user runs out of the max guess.Set a variable that gives you a set of numbers of guesses
-// else decrease remaining guess
-// if remaing guess = 0
-// then update loss to 1++
+
+// function starts below //
+document.onkeyup = function(event) {
+  var UserGuess = event.key;
+  AllUser_Guess.push(UserGuess);
+  //
+  // document.querySelector("#AllUser_Guess").innerHTML =AllUser_Guess;
+  RemainingGuess = RemainingGuess - 1;
+  // below is for win //
+  if (UserGuess === ComputerGuess) {
+    win = win + 1;
+    RemainingGuess = 5;
+    document.querySelector("#win_counter").innerHTML = win;
+    ComputerGuess = Computer_Choice_list[Math.floor(Math.random() * Computer_Choice_list.length)];
+    AllUser_Guess = [];
+  }
+
+  console.log(ComputerGuess);
+  // below is for remaining guess and display letters //
+  // also we are updating loss by +1
+
+
+  // Decreasing remaining guess by 1
+  if (RemainingGuess == 0) {
+    RemainingGuess = 5; // we are making it pick another
+    alert("Game restaring");
+    losses = losses + 1;
+    document.querySelector("#losses_counter").innerHTML = losses;
+    AllUser_Guess = [];
+    ComputerGuess = Computer_Choice_list[Math.floor(Math.random() * Computer_Choice_list.length)]; // retarting the game
+
+
+    // we are chanign the number //
+  }
+  document.querySelector("#RemainingGuess").innerHTML = RemainingGuess;
+  document.querySelector("#AllUser_Guess").innerHTML = AllUser_Guess;
+  // ----------------------------------------------------- //
 }
-else if (RemainingGuess<=3){
-  alert("I wanted to see if this works");
-}
-// Here keep the game going
-//while decreaing RemainingGuess by 1.
-else{
-alert("again trying to see if it works")
-// here I would say if
-
-// Problem 6: Update the actual HTML file
-// create a variable to show the actual guess using data from results of game. I am gone use a query selector
-
-
-var html =
-  "<p>Wins: " + win + "</p>" +
-  "<p>Losses:" + losses + "</p>" +
-  "<p> Guess left: " + RemainingGuess + "</p>";
-
-// Set the inner HTML contents of the #game div to our html string
-document.querySelector("#game").innerHTML = html;
-}
-
-
-};
